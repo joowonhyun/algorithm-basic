c02cd70afe1230e159428ea56763e7f5f747837a
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2025-07-15 22:13:43 +0900

    feat: 정렬 구현

---
+++ sort/insertion-sort.js
@@ -0,0 +1,17 @@
+function insertionSort(array) {
+  const n = array.length;
+
+  for (let i = 1; i < n; i++) {
+    let current = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > current) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = current;
+  }
+  return array;
+}
+
+console.log(insertionSort([2, 6, 10, 29, 50, 13]));
