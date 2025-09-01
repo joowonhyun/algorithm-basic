0873a12a76c5695e41b45979ffe7921f38b464c4
Joowon Hyun <78545341+joowonhyun@users.noreply.github.com>
2025-09-01 23:49:30 +0900

    Update 2.js

--- bfs/2.js
+++ bfs/2.js
@@ -1,50 +1,55 @@
 function solution(maps) {
-  const answer = [];
   const n = maps.length;
   const m = maps[0].length;
+  /**
+   * dx, dy 상우하좌
+   */
+  const dx = [-1, 0, 1, 0];
+  const dy = [0, 1, 0, -1];
 
-  const check = Array.from({ length: n }, () => Array(m).fill(0));
+  const d = Array.from({ length: n }, () => Array(m).fill(0));
+  const queue = [];
 
-  function bfs(maps, x, y) {
-    const dx = [-1, 1, 0, 0]; // x 변화 (행 이동)
-    const dy = [0, 0, 1, -1]; // y 변화 (열 이동)
-    const queue = [[x, y]];
-    let count = Number(maps[x][y]);
-    check[x][y] = 1;
+  queue.push([0, 0]);
+  d[0][0] = 1;
+  maps[0][0] = 0;
 
-    while (queue.length) {
-      const [x, y] = queue.shift();
-      for (let k = 0; k < 4; k++) {
-        const nx = x + dx[k];
-        const ny = y + dy[k];
-        if (
-          0 <= nx &&
-          nx < n &&
-          0 <= ny &&
-          ny < m &&
-          check[nx][ny] === 0 &&
-          maps[nx][ny] !== "X"
-        ) {
-          check[nx][ny] = 1;
-          queue.push([nx, ny]);
-          count += Number(maps[nx][ny]);
-        }
-      }
-    }
-    return count;
-  }
+  while (queue.length) {
+    let [x, y] = queue.shift();
+    for (let k = 0; k < 4; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
 
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      if (maps[i][j] !== "X" && check[i][j] === 0) {
-        const count = bfs(maps, i, j);
-        answer.push(count);
+      if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] === 1) {
+        queue.push([nx, ny]);
+        maps[nx][ny] = 0;
+        d[nx][ny] = d[x][y] + 1;
       }
     }
   }
-
-  return answer.length ? answer.sort((a, b) => a - b) : [-1];
+  return d[n - 1][m - 1] || -1;
 }
-console.log(solution(["X426X", "3X5X1", "XXX23", "7XX17"]));
-console.log(solution(["X1X2X", "X1X2X", "X1X2X"]));
-console.log(solution(["XXX", "XXX", "XXX"]));
+
+console.log(
+  solution([
+    [1, 1, 0, 1],
+    [0, 1, 0, 1],
+    [0, 1, 1, 1],
+    [1, 0, 0, 1],
+  ])
+);
+console.log(
+  solution([
+    [1, 0, 0],
+    [1, 0, 0],
+    [1, 1, 1],
+  ])
+);
+
+console.log(
+  solution([
+    [1, 0, 1],
+    [0, 0, 1],
+    [1, 1, 1],
+  ])
+);
